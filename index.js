9f9c4325a2353c8764bc2be08ef69988f0b5f581
bergurg <bergurg@vodafone.is>
2020-02-26 13:05:53 +0000

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+// @ts-check
+'use strict';
+
+const program = require('commander');
+const fs = require('fs');
+const util = require('util');
+let readFile = util.promisify(fs.readFile)
+
+program
+  .command('owner <pattern>')
+  .description('list the owners for pattern')
+  .action(async function(pattern, cmdObj) {
+    console.log(await loadOwners('CODEOWNERS', pattern));
+  });
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
+
+program.parse(process.argv);
+
+function parse(str, matchPattern) {
+    let entries = [];
+    let lines = str.split("\n");
+    for (let line of lines) {
+        let [content, comment] = line.split("#");
+        let trimmed = content.trim();
+        if (trimmed === "")
+            continue;
+        let [pattern, ...owners] = trimmed.split(/\s+/);
+        console.log(pattern)
+        if(pattern == matchPattern){
+          entries.push({ pattern, owners });
+        }
+    }
+    return entries.reverse();
+}
+
+async function loadOwners(cwd, matchPattern) {
+    let contents = await readFile(cwd, "utf-8");
+    let entries = parse(contents, matchPattern);
+    return entries;
+}
